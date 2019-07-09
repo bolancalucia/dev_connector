ff527966e6f38d99b2ff02f99631fe6fe0fcf5fb
bolancalucia <bolancalucia@gmail.com>
2019-07-09 18:30:51 +0200

    Deployment ready

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -26,6 +26,10 @@ const Profile = ({
         <Spinner />
       ) : (
         <Fragment>
+          <Link to='/profiles' className='btn btn-light'>
+            {' '}
+            Back To Profiles
+          </Link>
           {auth.isAuthenticated &&
             auth.loading === false &&
             auth.user._id === profile.user._id && (
@@ -71,10 +75,6 @@ const Profile = ({
               <ProfileGithub username={profile.githubusername} />
             )}
           </div>
-          <Link to='/profiles' className='btn btn-light'>
-            {' '}
-            Back To Profiles
-          </Link>
         </Fragment>
       )}
     </Fragment>
